52a76727a735a6058c2dbc05149b8b778430b2f9
Manjeet Kumar Mishra <mishramanjeet26@gmail.com>
2025-04-13 05:10:34 +0530

    Fix: Ensure Prisma Client is generated correctly for Vercel

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,6 +1,9 @@
-import prisma from '@/lib/prisma';
+import { PrismaClient } from '@prisma/client';
+
+
 import { NextResponse } from 'next/server';
 
+const prisma = new PrismaClient();
 export async function POST(request) {
   try {
     const { name, email, message } = await request.json();
